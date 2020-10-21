58eadc8ad7f6d1450d7522de7c4c8ff34d54f67e
Honoour <hearful@icloud.com>
2020-10-22 00:24:57 +0900

    Counting Sort

---
+++ src/Algorithm/CountingSort.java
@@ -0,0 +1,39 @@
+package Algorithm;
+
+public class CountingSort {
+
+    public CountingSort() {
+    }
+
+    public void Count(int[] A, int i, int k) {
+        int[] C = new int[k];
+        while(i < k) {
+            C[A[i]]++;
+            System.out.println("A["+ i +"] = " + A[i]);
+            System.out.println("C[A["+ i +"]] = " + C[A[i]]);
+            i++;
+        }
+        CumulativeSum(C, 0, C.length-1);
+    }
+
+    public void CumulativeSum(int[] C, int i, int k) {
+        int[] B = new int[C.length];
+        if (i > C.length-1) {
+            return;
+        }
+        if (i < 1) {
+            B[i] = C[i];
+            System.out.println("B["+ i +"] = " + B[i]);
+            System.out.println("C["+ i +"] = " + C[i]);
+            i++;
+        }
+        B[i] = B[i-1] + C[i];
+        System.out.println("B["+ i +"] = " + B[i]);
+        System.out.println("B["+ (i-1) +"] = " + B[i-1]);
+        System.out.println("C["+ i +"] = " + C[i]);
+        i++;
+        CumulativeSum(C, i, C.length-1);
+
+    }
+
+}
--- src/Algorithm/Main.java
+++ src/Algorithm/Main.java
@@ -3,6 +3,9 @@ package Algorithm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Counting Sort
+        int[] A = new int[] {0,2,1,2,2,3,1,1};
+        CountingSort countingSort = new CountingSort();
+        countingSort.Count(A, 0, 4);
     }
 }
