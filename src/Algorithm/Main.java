d856b0307e80e699c7f56712165c0b42c579be20
thekireii <thekireii@gmail.com>
2020-10-24 13:25:18 +0900

    revert

--- src/Algorithm/Main.java
+++ src/Algorithm/Main.java
@@ -7,8 +7,5 @@ public class Main {
         int[] A = new int[] {0,2,1,2,2,3,1,1};
         CountingSort countingSort = new CountingSort();
         countingSort.Count(A, 0, 4);
-
-        int data[] = { 5, 2, 1, 7, 112, 42, 52, 62 };
-        HeapSort h = new HeapSort(data);
     }
 }
