06978914da1360fca133cebd0348313888d248ae
HONOOUR <hearful@icloud.com>
2020-10-24 13:34:18 +0900

    Heap Sort

--- src/Algorithm/Main.java
+++ src/Algorithm/Main.java
@@ -3,9 +3,15 @@ package Algorithm;
 public class Main {
 
     public static void main(String[] args) {
-        // Counting Sort
+        System.out.println("Counting Sort starts.");
         int[] A = new int[] {0,2,1,2,2,3,1,1};
         CountingSort countingSort = new CountingSort();
         countingSort.Count(A, 0, 4);
+        System.out.println("Counting Sort ends.");
+
+        System.out.println("Heap Sort starts.");
+        int data[] = { 5, 2, 1, 7, 112, 42, 52, 62 };
+        HeapSort h = new HeapSort(data);
+        System.out.println("Heap Sort ends.");
     }
 }
